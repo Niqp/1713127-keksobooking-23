f569801a0cde8cff6c66b3f9bb8a926ce3be91d8
Nikita <hukull3@gmail.com>
2021-05-31 01:30:00 +0300

    Изменил функции на стрелочные

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // src = https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInteger(min, max) {
+const getRandomInteger = (min, max) => {
   if (min >= max) {
     throw new Error('Ошибка: минимальное значение больше или равно максимальному!');
   }
@@ -9,19 +9,18 @@ function getRandomInteger(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 // src = https://stackoverflow.com/a/42356278
-function getRandomFloat(min, max, precision) {
+const getRandomFloat = (min, max, precision) => {
   if (min >= max) {
     throw new Error('Ошибка: минимальное значение больше или равно максимальному!');
   }
   if (min<0 || max<0) {
     throw new Error('Ошибка: отрицательные значения не принимаются!');
   }
-  return Math.round(Math.random() * Math.pow(10, precision)) /
-            Math.pow(10, precision) * (max - min) + min;
-}
+  return Math.round(Math.random() * Math.pow(10, precision)) / Math.pow(10, precision) * (max - min) + min;
+};
 
-getRandomInteger(2,5);
-getRandomFloat(1.1,1.2,2);
+const randomInteger = getRandomInteger(2,5);
+const randomFloat = getRandomFloat(1.1,1.2,2);
