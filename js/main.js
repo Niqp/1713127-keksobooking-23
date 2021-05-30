20f55bbf6ff36673c1249b7f68edfa1a71b4a11c
Nikita <hukull3@gmail.com>
2021-05-30 12:57:45 +0300

    Добавил вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// src = https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInteger(min, max) {
+  if (min >= max) {
+    throw new Error('Ошибка: минимальное значение больше или равно максимальному!');
+  }
+  if (min<0 || max<0) {
+    throw new Error('Ошибка: отрицательные значения не принимаются!');
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// src = https://stackoverflow.com/a/42356278
+function getRandomFloat(min, max, precision) {
+  if (min >= max) {
+    throw new Error('Ошибка: минимальное значение больше или равно максимальному!');
+  }
+  if (min<0 || max<0) {
+    throw new Error('Ошибка: отрицательные значения не принимаются!');
+  }
+  return Math.round(Math.random() * Math.pow(10, precision)) /
+            Math.pow(10, precision) * (max - min) + min;
+}
+
+getRandomInteger(2,5);
+getRandomFloat(1.1,1.2,2);
